0ccaa60dfaba580e92129dd378f5402adf2cffa9
aprietof <adrianprieto00@gmail.com>
2016-09-29 13:40:01 -0400

    Testing diferent routes and rendering

--- js/app/app.js
+++ js/app/app.js
@@ -1,26 +1,25 @@
 angular
-    .module('app', ['ui.router'])
-    .config(function ($stateProvider,$urlRouterProvider) {
-      $stateProvider
-        .state('home', {
-          url: '/',
-          templateUrl: 'views/home.html',
-          controller: 'AppController'
-        })
-        .state('home.notifications', {
-          url: 'notifications',
-          templateUrl: 'views/home/notifications.html',
-          controller: 'AppController'
-        })
-        .state('home.timeline', {
-          url: 'timeline',
-          templateUrl: 'views/home/timeline.html',
-          controller: 'AppController'
-        })
-        .state('home.user', {
-          url: 'user',
-          templateUrl: 'views/home/user.html',
-          controller: 'AppController'
-        })
-      $urlRouterProvider.otherwise('/');
-    })
+  .module('app', ['ui.router'])
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html',
+        controller: 'AppController'
+      })
+      .state('home.notifications', {
+        url: '/notifications',
+        templateUrl: 'views/home/notifications.html',
+        controller: 'AppController'
+      })
+      .state('home.timeline', {
+        url: '/timeline',
+        templateUrl: 'views/home/timeline.html',
+        controller: 'AppController'
+      })
+      .state('home.user', {
+        url: '/user',
+        templateUrl: 'views/home/user.html',
+        controller: 'AppController'
+      })
+  })
